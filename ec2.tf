d562a8d04375f29ad40934b770d56ff564f852c4
rituraj07majumder <241207043+rituraj07majumder@users.noreply.github.com>
2026-06-25 22:25:17 +0100

    updating code

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,6 @@
-resource "aws_instance" "myec2" {
-    ami = "ami-00c39f71452c08778"
-    instance_type = "t2.micro"
+resource "aws_security_group" "example_sg" {
+  name        = "my-empty-sg"
+  tags = {
+    Name = "my-empty-sg"
+  }
 }

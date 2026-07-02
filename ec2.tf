87c3d53081e57a6f25756b5a39e5d6da629d7f14
rituraj07majumder <241207043+rituraj07majumder@users.noreply.github.com>
2026-07-02 21:35:58 +0100

    another sg creation

--- ec2.tf
+++ ec2.tf
@@ -2,3 +2,8 @@ resource "aws_security_group" "test_sg" {
   name        = "test-sg"
   description = "Minimal security group with no rules"
 }
+
+resource "aws_security_group" "test_sg02" {
+  name        = "test-sg02"
+  description = "Minimal security group with no rules"
+}

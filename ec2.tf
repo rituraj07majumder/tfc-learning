0ee99d39496dc91b1135041af925c14ce3799ade
rituraj07majumder <241207043+rituraj07majumder@users.noreply.github.com>
2026-07-02 23:43:46 +0100

    cleaning resources

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,1 @@
-resource "aws_security_group" "test_sg" {
-  name        = "test-sg"
-  description = "Minimal security group with no rules"
-}
 
-resource "aws_security_group" "test_sg02" {
-  name        = "test-sg02"
-  description = "Minimal security group with no rules"
-tags = {
-    "Cost Centre" = "12345"
-  }
-}

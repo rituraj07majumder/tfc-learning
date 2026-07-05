377bfa242076491563e57a277bf94c06011afda5
rituraj07majumder <241207043+rituraj07majumder@users.noreply.github.com>
2026-07-05 15:25:59 +0100

    sg01- private module

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,12 @@
+module "app_security_group" {
+  source  = "app.terraform.io/<YOUR_ORG>/<WORKSPACE_NAME>"
+  version = ">= 5.0"
 
+  name        = "test-sg-01"
+  description = "Security group for test01"
+
+#  tags = {
+#    Environment = "production"
+#    Team        = "backend"
+#  }
+}

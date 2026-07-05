e2a20f860149e62e70f2101aaa4974751afae4a1
unknown <rituraj07majumder@gmail.com>
2026-07-05 15:41:01 +0100

    first sg addition

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,13 @@
 
+module "app_security_group" {
+  source  = "app.terraform.io/rituraj07majumder/terraform-aws-sg"
+  version = ">= 5.0"
+
+  name        = "test-sg-01"
+  description = "Security group for test01"
+
+#  tags = {
+#    Environment = "production"
+#    Team        = "backend"
+#  }
+}

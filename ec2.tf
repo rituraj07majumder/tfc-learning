405c756b9f4b996593da7c92326ad31152a8f7e7
unknown <rituraj07majumder@gmail.com>
2026-07-05 19:46:55 +0100

    destroying stuff

--- ec2.tf
+++ ec2.tf
@@ -1,26 +1,26 @@
 
-module "terraform-aws-sg" {
-  source  = "app.terraform.io/raj-org1/terraform-aws-sg/aws"
-  version = ">=1.0.0"
+# module "terraform-aws-sg" {
+#   source  = "app.terraform.io/raj-org1/terraform-aws-sg/aws"
+#   version = ">=1.0.0"
 
-  name        = "test-sg-01"
-  description = "Security group for test01"
+#   name        = "test-sg-01"
+#   description = "Security group for test01"
 
-  tags = {
-    Environment = "production"
-    "Cost Centre" = "12345"
-  }
-}
+#   tags = {
+#     Environment = "production"
+#     "Cost Centre" = "12345"
+#   }
+# }
 
 
-module "test-sg-02" {
-  source  = "app.terraform.io/raj-org1/terraform-aws-sg/aws"
-  version = ">=1.0.0"
+# module "test-sg-02" {
+#   source  = "app.terraform.io/raj-org1/terraform-aws-sg/aws"
+#   version = ">=1.0.0"
 
-  name        = "test-sg-02"
-  description = "Security group for test02"
+#   name        = "test-sg-02"
+#   description = "Security group for test02"
 
-  tags = {
-    "Cost Centre" = "12348"
-  }
-}
+#   tags = {
+#     "Cost Centre" = "12348"
+#   }
+# }

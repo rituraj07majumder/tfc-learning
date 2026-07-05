212ad9a7466040cb66c37131cc56f356a5faa267
unknown <rituraj07majumder@gmail.com>
2026-07-05 16:37:21 +0100

    fixing module

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 
-module "this" {
+module "terraform-aws-sg" {
   source  = "app.terraform.io/rituraj07majumder/sg/aws"
   version = ">= 1.0"
 
@@ -11,3 +11,9 @@ module "this" {
 #    Team        = "backend"
   }
 }
+
+# module "terraform-aws-sg" {
+#   source  = "app.terraform.io/raj-org1/terraform-aws-sg/aws"
+#   version = "1.0.0"
+#   # insert required variables here
+# }

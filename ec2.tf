0d5801fa7307ca95a6edc2c14056297fab1ba8e3
unknown <rituraj07majumder@gmail.com>
2026-07-05 16:40:28 +0100

    fixing module 03

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 
 module "terraform-aws-sg" {
-  source  = "app.terraform.io/raj-org1/sg/aws"
-  version = ">= 1.0"
+  source  = "app.terraform.io/raj-org1/terraform-aws-sg/aws"
+  version = "1.0.0"
 
   name        = "test-sg-01"
   description = "Security group for test01"
